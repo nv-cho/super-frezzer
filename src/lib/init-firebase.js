843073239ca6a91c078e2be6596285c5520f72d4
cuouz <cu.ouza@gmail.com>
2022-06-23 23:36:00 -0300

    deleting variables

--- src/lib/init-firebase.js
+++ src/lib/init-firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzKwY0mvA8b9NxuSchqE7GSo0qNzzYSg8",
-  authDomain: "basedatosfreezer-95d08.firebaseapp.com",
-  projectId: "basedatosfreezer-95d08",
-  storageBucket: "basedatosfreezer-95d08.appspot.com",
-  messagingSenderId: "235960279765",
-  appId: "1:235960279765:web:d8e1751a53d0cebcc7f85c",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
